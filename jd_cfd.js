857f4ae41b1e7593b6e5893799fcde1585640215
Cuteribs <ericfine1981@live.com>
2021-06-18 17:46:15 +0800

    Update jd_cfd.js

--- jd_cfd.js
+++ jd_cfd.js
@@ -1128,6 +1128,8 @@ function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
+	  resolve(null);
+	  return;
     $.get({
       url: `http://share.turinglabs.net/api/v3/jxcfd/query/${randomCount}/`,
       'timeout': 10000
@@ -1162,7 +1164,7 @@ function shareCodesFormat() {
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       // const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
-      $.newShareCodes = [...$.strMyShareIds, "F45CB4F07997DFE748E5656521A9034446A1568F6950206B0D44A5664662275D"];
+      $.newShareCodes = [...$.strMyShareIds];
     }
     const readShareCodeRes = await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
